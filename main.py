8107eaff88e400b282f044ed1d9cecff14e9aa70
ziyodullo <ziyodullodev@gmail.com>
2025-09-26 17:25:35 +0500

    fsad

--- main.py
+++ main.py
@@ -2,3 +2,27 @@ print("salom")
 
 
 print("alik")
+
+
+
+# boshida
+# create repositry
+# copy link
+# open vscode and terminal
+# ls, mkdir, cd
+# git clone [link]
+# ls
+# cd [link]
+# code . | papkalardan o'zimiz qolda tanlaymiz
+
+
+# ozgarishlarni yuborish uchun
+# git add .
+# git commit -m "nimadur"
+# git push
+
+
+# ozgarishlarni qabul qilish
+# git pull
+
+
